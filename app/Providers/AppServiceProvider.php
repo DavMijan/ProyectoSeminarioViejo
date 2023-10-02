0f89f960d400775180ae5ad800b476c746911abd
DavMijan <davmijan99@gmail.com>
2023-10-01 18:32:13 -0600

    Proyecto Base

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+        Gate::define('admins', function () {
+             if(Auth::user()->hasRole('admin')){
+                return true;
+            }
+            return false;
+        });
+        Gate::define('standard', function () {
+           if(Auth::user()->hasRole('stand')){
+                return true;
+            }
+            return false;
+        });
+    }
+}
